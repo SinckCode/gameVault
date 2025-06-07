a019e6d56c1cc001dc6715552f471e905b36d88c
SinckCode <sinckinfo@gmail.com>
2025-06-06 20:02:07 -0600

    Proyecto Corregido

--- gameVault/ContentView.swift
+++ gameVault/ContentView.swift
@@ -1,21 +1,65 @@
-//
-//  ContentView.swift
-//  gameVault
-//
-//  Created by Onesto on 06/06/25.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack(alignment: .bottom) {
+            VStack(spacing: 0) {
+                
+                // Barra superior
+                HStack {
+                    Text("Game Vault")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                    
+                    Spacer()
+                    
+                    Image(systemName: "person.circle.fill")
+                        .resizable()
+                        .frame(width: 40, height: 40)
+                        .foregroundColor(.blue)
+                }
+                .padding()
+                
+                
+                
+                // Barra inferior fija
+                TabBarView()
+                    .padding(.horizontal)
+                    .padding(.bottom, 10)
+            }
+        }
+    }
+}
+
+
+
+struct TabBarView: View {
+    var body: some View {
+        HStack {
+            Spacer()
+            VStack {
+                Image(systemName: "house.fill")
+                Text("Inicio")
+                    .font(.caption)
+            }
+            Spacer()
+            VStack {
+                Image(systemName: "magnifyingglass")
+                Text("Buscar")
+                    .font(.caption)
+            }
+            Spacer()
+            VStack {
+                Image(systemName: "star.fill")
+                Text("Favoritos")
+                    .font(.caption)
+            }
+            Spacer()
         }
         .padding()
+        .background(Color.blue.opacity(0.95))
+        .foregroundColor(.white)
+        .cornerRadius(15)
     }
 }
 
