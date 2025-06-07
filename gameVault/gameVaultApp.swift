1c48e63a59a7c92474ef015b47d0b37627efba1e
SinckCode <sinckinfo@gmail.com>
2025-06-06 19:55:58 -0600

    Initial Commit

---
+++ gameVault/gameVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  gameVaultApp.swift
+//  gameVault
+//
+//  Created by Onesto on 06/06/25.
+//
+
+import SwiftUI
+
+@main
+struct gameVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ gameVaultTests/gameVaultTests.swift
@@ -0,0 +1,16 @@
+//
+//  gameVaultTests.swift
+//  gameVaultTests
+//
+//  Created by Onesto on 06/06/25.
+//
+
+import Testing
+
+struct gameVaultTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
